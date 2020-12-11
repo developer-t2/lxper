5d7b17bb572615ad3a0c3eeea85ce9bf3d3f4f44
t2 <oo_ri@naver.com>
2020-12-11 11:53:34 +0900

    Material-UI Initial Setting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+
+import theme from './theme';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.querySelector('#root')
 );
